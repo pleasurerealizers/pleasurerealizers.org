ab1de3654315ba2cad24395ecac4212760c73369
Julian Nalenz <julian@pleasurerealizers.org>
2024-06-16 04:39:28 +0200

    feat: update content config, add sitemap

--- content/config.mjs
+++ content/config.mjs
@@ -4,9 +4,16 @@
  */
 export default {
   title: "Pleasure Realizers",
+  author: "Pleasure Realizers",
+  authorUrl: "https://pleasurerealizers.org",
+  analytics: "G-T02Z3W0LSL",
   description: "Building bridges between reality and reflected utopias.",
-  domain: "https://pleasurerealizers.org/",
+  domain: "https://pleasurerealizers.org",
   navbarTitle: {
     text: "Pleasure Realizers"
-  }
+  },
+  socialLinks: {
+    instagram: "https://www.instagram.com/pleasurerealizers/",
+  },
+  theme: { default: "" },
 };
