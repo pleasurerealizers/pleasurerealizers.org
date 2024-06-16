4ed5d5775f994bfaa3245de466befbcbb5ace783
Julian Nalenz <julian@pleasurerealizers.org>
2024-06-16 04:21:05 +0200

    feat: customizations for Pleasure Realizers

--- content/config.mjs
+++ content/config.mjs
@@ -2,4 +2,11 @@
 /**
  * @type {import("../config/siteConfig").UserConfig}
  */
-export default {};
+export default {
+  title: "Pleasure Realizers",
+  description: "Building bridges between reality and reflected utopias.",
+  domain: "https://pleasurerealizers.org/",
+  navbarTitle: {
+    text: "Pleasure Realizers"
+  }
+};
